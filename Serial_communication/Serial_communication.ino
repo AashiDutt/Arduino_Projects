990dabad9a241366b7f20bad98862f38ce006945
Aashi Dutt <aashi.dutt3@gmail.com>
2018-07-10 22:13:07 +0530

    Add files via upload

---
+++ Serial_communication/Serial_communication.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  // code for serial communication
+Serial.begin(9600);  // 9600 bits per second (speed of communication)
+Serial.println("starting");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("A "); //used to transit letters for comm.
+  delay(1000);
+  Serial.println("B");
+  delay(1000);
+}
