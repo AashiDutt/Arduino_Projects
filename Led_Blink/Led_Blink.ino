1992e50b4d0e2ce4519f5c0a554c3e9bc1200b96
Aashi Dutt <aashi.dutt3@gmail.com>
2018-07-10 11:02:05 +0530

    Add files via upload

---
+++ Led_Blink/Led_Blink.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13,OUTPUT); // defines pin 13 as output pin used in o/p mode
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH); // writing value high to pin 13 ( +5 V)
+  delay(250);           // creates delay of 1s i.e 1000ms
+  digitalWrite(13,LOW);
+  delay(250);
+}
