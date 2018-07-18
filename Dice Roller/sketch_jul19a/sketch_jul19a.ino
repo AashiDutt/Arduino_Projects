e7e038d20a29ce3216badb7d54585238300825ae
Aashi Dutt <aashi.dutt3@gmail.com>
2018-07-19 01:16:45 +0530

    Add files via upload

---
+++ Dice Roller/sketch_jul19a/sketch_jul19a.ino
@@ -0,0 +1,30 @@
+void setup() {
+  // put your setup code here, to run once:
+   Serial.begin(9600);
+
+   // seed our random number generator
+   randomSeed(analogRead(A0));
+}
+
+void loop() {
+  String str;
+  int num;
+  
+  Serial.println("Enter the number of sides on the die");
+  while(str.equals("")){
+    str =Serial.readString();
+  }
+
+  // convert input string into number
+  num = str.toInt();
+
+  // only roll the die if we have a positive number
+
+  if ( num > 0){
+    num = random(1, num+1);
+    Serial.print("You rolled : ");
+    Serial.println(num);
+     
+  }
+  
+}
