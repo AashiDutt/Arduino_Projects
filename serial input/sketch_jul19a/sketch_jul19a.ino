8df1a0c324c1123944143f826c209ae2e7f606c4
Aashi Dutt <aashi.dutt3@gmail.com>
2018-07-19 00:55:41 +0530

    Add files via upload

---
+++ serial input/sketch_jul19a/sketch_jul19a.ino
@@ -0,0 +1,34 @@
+// READING SERIAL INPUT
+
+void setup() {
+  Serial.begin(9600); 
+}
+
+void loop() {
+  
+String fname = "";  // create empty strings
+String lname = "";
+
+Serial.println("Enter your first name");
+
+// if string is still empty read the new string 
+// else break out of the loop
+
+while( fname.equals("")){
+  fname = Serial.readString();
+}
+
+ Serial.println("enter your last name");
+ while( lname.equals("")){
+  lname = Serial.readString();
+ }
+ 
+ // concatinating first and last name
+ fname.concat(" ");
+ fname.concat(lname);
+
+// Print the output
+ Serial.print("Hello, ");
+ Serial.println(fname);
+ Serial.println();  
+}
