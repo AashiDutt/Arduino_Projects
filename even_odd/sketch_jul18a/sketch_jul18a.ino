1fac8ad6a69ad03d2fdf2b39f2006535df9afa9d
Aashi Dutt <aashi.dutt3@gmail.com>
2018-07-18 09:11:20 +0530

    Add files via upload

---
+++ even_odd/sketch_jul18a/sketch_jul18a.ino
@@ -0,0 +1,34 @@
+
+// This code represents even and odd numbers 
+// writing user defined  function
+int i = 0;
+int e;
+
+void setup() {
+   Serial.begin(9600);
+}
+
+void loop() {
+  e = isEven(i);
+  
+  Serial.print(i);
+  Serial.print(": ");
+  if(e==1){
+    Serial.println("even");
+  } else{
+    Serial.println("odd");
+  }
+  i++;
+
+  delay(500);
+}
+
+int isEven(int a){
+
+  if( a % 2 == 0){
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
