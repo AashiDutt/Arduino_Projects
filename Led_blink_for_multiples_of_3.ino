edd6984cddb80e84b037b346a606415f75531d93
Aashi Dutt <aashi.dutt3@gmail.com>
2018-07-20 08:23:20 +0530

    Add files via upload

---
+++ Led_blink_for_multiples_of_3.ino
@@ -0,0 +1,28 @@
+// Write an Arduino program that prints integers that
+//count up from 1,
+//except that for every multiple of 3 (3, 6, 9, 12, etc.),
+//an LED attached to pin 5 flashes on for 0.5 seconds.
+// ckt link below
+https://www.tinkercad.com/things/2eg9DQvm5Fx-mighty-stantia-hillar/editel
+
+
+int i;
+const int led= 13;
+void setup() {
+  Serial.begin(9600);
+  pinMode(led , OUTPUT);
+}
+
+void loop() {
+  for(i=1;i<30;i++){
+   if(i % 3 == 0){
+    digitalWrite(led , HIGH);
+    delay(500);
+    digitalWrite(led , LOW);
+    delay(500);
+   }else{
+    Serial.println(i);
+   }
+  }
+
+}
