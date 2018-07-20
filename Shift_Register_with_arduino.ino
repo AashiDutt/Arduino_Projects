0c7749cff912e9ebc7e8afc68b680f46717912c1
Aashi Dutt <aashi.dutt3@gmail.com>
2018-07-20 11:54:48 +0530

    Add files via upload

---
+++ Shift_Register_with_arduino.ino
@@ -0,0 +1,42 @@
+// Interfacing Arduino with Shift Register
+// link to circuit
+// https://www.tinkercad.com/things/cnafqzJ61rp-sizzling-blorr/editel?tenant=circuits
+
+// Pins
+const int data_pin =2;
+const int clock_pin = 3;
+const int latch_pin = 4;
+
+void setup() {
+  pinMode(data_pin ,OUTPUT);
+  pinMode(clock_pin, OUTPUT);
+  pinMode(latch_pin , OUTPUT);
+  
+}
+
+void loop() {
+  // B = binary number
+  byte x =B10000000;
+
+  for(int i =0;i<8;i++){
+    shiftDisplay(x);
+    x = x >>1; // bitwise right shift x on every iteration
+    delay(200);
+  }
+
+}
+
+
+// for transfer to take place 1st latch is initiated to low indictes a signal is about to come
+// on completion of transfer turn latch pin to HIGH
+
+void shiftDisplay(byte data){
+  digitalWrite(latch_pin, LOW);
+ // shiftout is a built-In function that sends a single byte serially n a pin.
+ // LSBFIRST = Least Significant Bit First
+ // MSBFIRST =Most Significant Bit First
+ 
+  shiftOut(data_pin, clock_pin,LSBFIRST , data);
+  digitalWrite(latch_pin, HIGH);
+}
+
