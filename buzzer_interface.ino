2676eba943f8ed400c398dba1bb81d2cba2e81c8
Aashi Dutt <aashi.dutt3@gmail.com>
2018-07-21 22:41:11 +0530

    Add files via upload

---
+++ buzzer_interface.ino
@@ -0,0 +1,14 @@
+// this is  a code for buzzer interface with arduino
+// link to simulator
+// https://www.tinkercad.com/things/2NId0mY2jjV-buzzer-interface
+const int speaker_pin = 9;
+void setup() {
+  pinMode(speaker_pin , OUTPUT);
+}
+
+void loop() {
+  tone(speaker_pin,440,500);
+  // (pin, frequncy in Hz,length of note (ms))
+  delay(1000);
+
+}
