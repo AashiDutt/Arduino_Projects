f0351ca95a1b27e6eda08f0d68792c9baa7cd120
Aashi Dutt <aashi.dutt3@gmail.com>
2018-07-22 12:05:17 +0530

    Add files via upload

---
+++ Light_Sensor.ino
@@ -0,0 +1,20 @@
+
+// Light Sensor with arduino
+
+const int light_pin = A0;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  
+  int light_val = analogRead(light_pin);
+
+  // returns value between 0 -1023 (0V -5V)
+
+  Serial.println(light_val);
+  delay(100);
+
+}
