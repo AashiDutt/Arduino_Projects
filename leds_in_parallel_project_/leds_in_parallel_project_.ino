c56bf7be3ebfe418ea19dd7f4a6ef7d158e53537
Aashi Dutt <aashi.dutt3@gmail.com>
2018-07-30 22:48:22 +0530

    Add files via upload

---
+++ leds_in_parallel_project_/leds_in_parallel_project_.ino
@@ -0,0 +1,17 @@
+// Use 1 GPIO pin on the microcontroller to blink 5 LEDs on 
+// and off simultaneously.
+// link to simulator
+// https://www.tinkercad.com/things/35ehdg3LtM5-leds-in-parallel-project
+
+const int led_pin = 6;
+ 
+void setup() {
+  pinMode(led_pin, OUTPUT);
+}
+ 
+void loop() {
+  digitalWrite(led_pin, HIGH);
+  delay(500);
+  digitalWrite(led_pin, LOW);
+  delay(500);
+}
