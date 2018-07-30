e19f4719bd1704e6e0776d8c735feb83d6569f68
Aashi Dutt <aashi.dutt3@gmail.com>
2018-07-30 21:41:42 +0530

    Add files via upload

---
+++ transistor_interface_arduino.ino
@@ -0,0 +1,19 @@
+// In this project we will be using a transistor to controll blinking of led
+// link to simulator 
+// https://www.tinkercad.com/things/j0lTBx60uLw-transistor-interface-using-arduino
+
+
+const int led_pin =6;
+
+void setup() {
+  pinMode(led_pin , OUTPUT);
+
+}
+
+void loop() {
+ digitalWrite(led_pin, HIGH);
+ delay(500);
+ digitalWrite(led_pin, LOW);
+ delay(500);
+
+}
