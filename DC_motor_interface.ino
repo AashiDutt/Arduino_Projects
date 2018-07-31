fa5958032720e7750315682327d4895ac67cef9f
Aashi Dutt <aashi.dutt3@gmail.com>
2018-07-31 06:41:44 +0530

    Add files via upload

---
+++ DC_motor_interface.ino
@@ -0,0 +1,17 @@
+
+// This code is an interfacing of arduino with vibrational motor.
+// link to simulator
+// https://www.tinkercad.com/things/8xtQyXLHPBP-dcmotorinterface
+
+const int motor = 6;
+void setup() {
+  pinMode(motor,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(motor ,HIGH);
+  delay(500);
+  digitalWrite(motor, LOW);
+  delay(500);
+
+}
