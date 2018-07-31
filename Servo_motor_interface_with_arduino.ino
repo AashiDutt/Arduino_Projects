2d55459c54aefcb2aff8b7679a39dff2dc6a4fed
Aashi Dutt <aashi.dutt3@gmail.com>
2018-07-31 21:27:22 +0530

    Add files via upload

---
+++ Servo_motor_interface_with_arduino.ino
@@ -0,0 +1,31 @@
+// This is a code for Servo Motor Interface with Arduino
+// link to simulator
+// 
+
+#include <Servo.h>
+
+const int servo_pin = 9;
+Servo servo;
+void setup() {
+// Attaching servo to pin 9
+  servo.attach(servo_pin);  
+}
+
+void loop() {
+  servo.write(90);  // sends appropriate signals for servo to move to center
+  delay(1000);
+
+  servo.write(180); // moves servo clockwise
+  delay(1000);
+
+  servo.write(0);   // moves servo counter clockwise
+  delay(1000);
+
+
+// sweeping servos from 0 to 180 degree at increment of 5 degree at each iteration.
+
+  for(int pos = 0; pos<=180; pos += 5){
+    servo.write(pos);
+    delay(50);
+  }
+}
