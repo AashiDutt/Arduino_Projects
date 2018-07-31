e9c6c08f16ec9236c2aa1b5427752ef134f9c5ad
Aashi Dutt <aashi.dutt3@gmail.com>
2018-07-31 20:27:23 +0530

    Add files via upload

---
+++ H_bridge_dc_motor.ino
@@ -0,0 +1,35 @@
+const int pwm_pin =9;
+const int dir_1a_pin = 8;
+const int dir_2a_pin = 7;
+
+void setup() {
+  pinMode(pwm_pin, OUTPUT);
+  pinMode(dir_1a_pin,OUTPUT);
+  pinMode(dir_2a_pin,OUTPUT);
+  
+}
+
+void loop() {
+
+  // one direction at half speed
+  digitalWrite(dir_1a_pin, HIGH);
+  digitalWrite(dir_2a_pin, LOW);
+  analogWrite(pwm_pin, 127);
+  delay(1000);
+
+  // stop spinning
+  analogWrite(pwm_pin, 0);
+  delay(1000);
+
+  // other direction at full speed
+  digitalWrite(dir_1a_pin, LOW);
+  digitalWrite(dir_2a_pin , HIGH);
+  analogWrite(pwm_pin, 255);
+  delay(10000);
+
+  // Brake
+  digitalWrite(dir_1a_pin,LOW);
+  digitalWrite(dir_2a_pin,LOW);
+  analogWrite(pwm_pin, 0);
+  delay(1000);
+}
