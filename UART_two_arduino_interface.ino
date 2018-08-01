0fd113f02d4ce6e873f023bd1cbcd1b677b9b23c
Aashi Dutt <aashi.dutt3@gmail.com>
2018-08-01 21:39:09 +0530

    Add files via upload

---
+++ UART_two_arduino_interface.ino
@@ -0,0 +1,18 @@
+// This code is UART(Universal Asynchronous Reciever Transmitter)
+// with two arduino interface.
+// link to simulator
+// https://www.tinkercad.com/things/ejs7skzYhHK-uarttwoarduinointerface
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial soft(8,9);  // Rx , Tx
+
+void setup() {
+  soft.begin(9600); // baud rate
+
+}
+
+void loop() {
+  soft.println("Hi");
+  delay(1000);  
+}
