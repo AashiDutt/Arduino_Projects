b4b8e4127451aedaf220b223617f84f46418e577
Aashi Dutt <aashi.dutt3@gmail.com>
2018-08-04 12:10:14 +0530

    Add files via upload

---
+++ Fade_Knob_With_potentiometer/Fade_Knob_With_potentiometer.ino
@@ -0,0 +1,23 @@
+// code to fade led glow with potentiometer
+// link to simulator
+https://www.tinkercad.com/things/dEE3EoobgQ7-fadeknobwithpotentiometer/editel
+
+// Pins
+const int knob_pin = A0;
+const int led_pin = 5;
+ 
+void setup() {
+  pinMode(led_pin, OUTPUT);
+}
+ 
+void loop() {
+  
+  // Read knob value (0 - 1023)
+  int val = analogRead(knob_pin);
+  
+  // Convert knob value to PWM value (0 - 255)
+  int brightness = map(val, 0, 1023, 0, 255);
+  
+  // Set LED brightness
+  analogWrite(led_pin, brightness);
+}
