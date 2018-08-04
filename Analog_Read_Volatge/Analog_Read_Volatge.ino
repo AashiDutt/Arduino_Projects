b823e5ebf34e24166ca490ad77064af9d28f3a7f
Aashi Dutt <aashi.dutt3@gmail.com>
2018-08-04 11:54:53 +0530

    Add files via upload

---
+++ Analog_Read_Volatge/Analog_Read_Volatge.ino
@@ -0,0 +1,19 @@
+// code to check volatge of a volatage divider circuit
+// link to simulator
+// https://www.tinkercad.com/things/7902QspGWiw-reading-analog-voltage/editel
+
+
+const int ain_pin =A0;
+
+void setup() {
+ Serial.begin(9600);
+ 
+}
+
+void loop() {
+  int val = analogRead(ain_pin);
+  float volatage = val * 5.0 / 1023;
+
+  Serial.println(voltage);
+  delay(100);
+}
