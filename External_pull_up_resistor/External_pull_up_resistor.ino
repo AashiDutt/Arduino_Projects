7df288dc436aef36b0f633c4e3c421595463fd4c
Aashi Dutt <aashi.dutt3@gmail.com>
2018-08-04 09:42:06 +0530

    Add files via upload

---
+++ External_pull_up_resistor/External_pull_up_resistor.ino
@@ -0,0 +1,20 @@
+const int btn_pin = 2;
+const int led_pin = 6;
+
+
+void setup() {
+  pinMode(btn_pin, INPUT);
+  pinMode(led_pin, OUTPUT);
+  
+}
+
+void loop() {
+  if( digitalRead(btn_pin) == LOW ){
+    // returns 0 for 0V  Button Pressed
+    // returns 1 for 5V  Button Not Pressed
+    digitalWrite(led_pin, HIGH);
+    }else {
+      digitalWrite(led_pin, LOW);
+    }
+
+}
